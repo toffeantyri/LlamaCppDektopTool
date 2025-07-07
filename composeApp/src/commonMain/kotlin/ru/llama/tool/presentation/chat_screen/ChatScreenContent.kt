75435954e779ce2337148518096d2a2066dfb3da
toffeantyri@gmail.com <toffeantyri@gmail.com>
2025-07-07 21:50:54 +0500

    chat screen - added top app bar

--- composeApp/src/commonMain/kotlin/ru/llama/tool/presentation/chat_screen/ChatScreenContent.kt
+++ composeApp/src/commonMain/kotlin/ru/llama/tool/presentation/chat_screen/ChatScreenContent.kt
@@ -5,9 +5,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,24 +27,40 @@ import llamacppdektoptool.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
 import ru.llama.tool.getPlatform
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatScreenContent(component: ChatComponent) {
-    Column(
-        modifier = Modifier
-            .safeContentPadding()
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        var showContent by remember { mutableStateOf(false) }
-        Button(onClick = { showContent = !showContent }) {
-            Text("Click me!")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Chat") },
+                navigationIcon = {
+                    IconButton(onClick = { /* TODO: Handle menu click */ }) {
+                        Icon(Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                }
+            )
         }
-        AnimatedVisibility(showContent) {
-            val greeting = remember { getPlatform() }
-            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                Image(painterResource(Res.drawable.compose_multiplatform), null)
-                Text("Compose: $greeting")
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            var showContent by remember { mutableStateOf(false) }
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { getPlatform() }
+                Column(
+                    Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting")
+                }
             }
         }
     }
