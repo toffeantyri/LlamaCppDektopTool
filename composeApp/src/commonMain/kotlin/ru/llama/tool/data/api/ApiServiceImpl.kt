416f6ffc392cd6909d060d8ebbadbdaee0d06852
toffeantyri@gmail.com <toffeantyri@gmail.com>
2025-07-11 18:37:41 +0500

    update timeout

--- composeApp/src/commonMain/kotlin/ru/llama/tool/data/api/ApiServiceImpl.kt
+++ composeApp/src/commonMain/kotlin/ru/llama/tool/data/api/ApiServiceImpl.kt
@@ -6,11 +6,13 @@ import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsChannel
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
-import io.ktor.utils.io.InternalAPI
 import io.ktor.utils.io.readUTF8Line
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.yield
 import kotlinx.serialization.json.Json
 import ru.llama.tool.data.api.models.llama_models.LLamaMessageDto
 import ru.llama.tool.data.api.models.llama_models.LlamaResponseDto
@@ -28,70 +30,73 @@ class ApiServiceImpl(
     private val client: HttpClient
 ) : ApiService {
 
-//    override suspend fun simpleRequestAi(message: MessageRequest): Flow<LlamaResponseDto> = flow {
-//        val url = "http://127.0.0.1:8080/v1/chat/completions"
-//
-//        val response = client.post(url) {
-//            contentType(ContentType.Application.Json)
-//            setBody(
-//                LLamaMessageDto(
-//                    messages = listOf(message),
-//                    stream = true
-//                )
-//            )
-//        }
-//
-//
-//        val input = response.bodyAsChannel()
-//        val packet = input.readRemaining(4096)
-//        var partial = ""
-//
-//        var counter = 0
-//
-//        while (!packet.exhausted()) {
-//            val bytes = packet.readByteArray()
-//            val text = bytes.decodeToString()
-//            val combined = partial + text
-//
-//            val lines = combined.split("\n")
-//
-//            partial = lines.last()
-//
-//            for (line in lines.dropLast(1)) {
-//                if (line.startsWith(" ")) {
-//                    val jsonString = line.substringAfter(" ").trim()
-//                    if (jsonString == "[DONE]") continue
-//
-//                    println("Count ${++counter}")
-//                    try {
-//                        val dto = json.decodeFromString<LlamaResponseDto>(line)
-//                        emit(dto)
-//                    } catch (e: Exception) {
-//                        println("Error 1 $e")
-//                    }
-//
-//                }
-//            }
-//        }
-//
-//
-//        if (partial.isNotBlank()) {
-//
-//            val jsonString = partial.trim()
-//            if (jsonString != "[DONE]") {
-//
-//                try {
-//                    val dto = json.decodeFromString<LlamaResponseDto>(partial)
-//                    emit(dto)
-////                println(dto.choices[0].delta.content)
-//                } catch (e: Exception) {
-//                    println("Error 2 $e")
-//                }
-//            }
-//        }
-//    }
 
-    @OptIn(InternalAPI::class)
+    suspend fun fullEmit(message: MessageRequest): Flow<Message> = flow {
+        val url = "http://127.0.0.1:8080/v1/chat/completions"
+
+        val response = client.post(url) {
+            contentType(ContentType.Application.Json)
+            setBody(
+                LLamaMessageDto(
+                    messages = listOf(message),
+                    stream = true
+                )
+            )
+        }
+
+        require(response.contentType()?.withoutParameters() == ContentType.Text.EventStream) {
+            "Expected text/event-stream but got ${response.contentType()}"
+        }
+
+        val content = response.bodyAsChannel()
+
+        var dataBuilder = StringBuilder()
+        var counter = 0
+
+        while (!content.isClosedForRead) {
+            val line = content.readUTF8Line() ?: continue
+
+            if (line.startsWith("data:")) {
+                val jsonString = line.substringAfter(":").trim()
+                if (jsonString == "[DONE]") {
+                    emit(
+                        Message(
+                            content = "",
+                            sender = EnumSender.AI,
+                            id = message.id
+                        )
+                    )
+                } else {
+                    val data =
+                        json.decodeFromString<LlamaResponseDto>(jsonString).choices[0].delta.content
+                            ?: ""
+                    dataBuilder.clear()
+                    dataBuilder.append(data)
+                }
+            } else if (line.isBlank() && dataBuilder.isNotEmpty()) {
+                emit(
+                    Message(
+                        content = dataBuilder.toString(),
+                        sender = EnumSender.AI,
+                        id = message.id
+                    )
+                )
+                dataBuilder.clear()
+                yield()
+            }
+        }
+
+        if (dataBuilder.isNotEmpty()) {
+            emit(
+                Message(
+                    content = dataBuilder.toString(),
+                    sender = EnumSender.AI,
+                    id = message.id
+                )
+            )
+        }
+    }.flowOn(Dispatchers.IO)
+
     override suspend fun simpleRequestAi(message: MessageRequest): Flow<Message> = flow {
         val url = "http://127.0.0.1:8080/v1/chat/completions"
 
@@ -141,15 +146,6 @@ class ApiServiceImpl(
                     )
                 }
             }
-//            else if (line.isBlank() && dataBuilder.isNotEmpty()) {
-//                emit(
-//                    Message(
-//                        content = dataBuilder.toString(),
-//                        sender = EnumSender.AI,
-//                        id = message.id
-//                    )
-//                )
-//            }
             delay(10)
         }
 
@@ -165,4 +161,6 @@ class ApiServiceImpl(
 
 
     }
+
+
 }
